4db7274ff728145605e26f40f7560955222c045f
Denis <denis_hara@hotmail.com>
2022-12-28 11:09:58 -0300

    ufa

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -7,6 +7,43 @@ namespace Models
 {
     public class Pessoa
     {
+
+        private string _nome;
+        public string Nome
+        {
+
         
+
+         get
+            {
+                return _nome.ToUpper();
+                //retornar o nome maiusculo
+            }
+
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio.");
+                    //não permite vazio.
+                }
+
+                _nome = value;
+
+            }
+
+
+
+        }
+
+
+
+
+
+        public int Idade { get; set; }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Meu nome é {Nome} e tenho {Idade}.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿using Models;
 
+Pessoa p1 = new Pessoa();
+
+p1.Idade = -1;
+
+p1.Nome = "Denis";
+p1.Apresentar();
+
+//Get obtendo valor armazenado nessa propriedade como ex:{Idade}. Set = setando/ atribuindo valor ex. = 25.
+
