ef8f4530e47d7d2b33efd04359ac91e33741635c
Denis <denis_hara@hotmail.com>
2023-01-02 09:57:58 -0300

    constr

--- Program.cs
+++ Program.cs
@@ -12,7 +12,37 @@ cursoDeIngles.AdicionarAlunos(p1);
 cursoDeIngles.AdicionarAlunos(p2);
 cursoDeIngles.ListarAlunos();
 
+/*
+construtor: atribuir valores para um objeto
+Get obtendo valor armazenado nessa propriedade como ex:{Idade}. Set = setando/ atribuindo valor ex. = 25.
+
+Quando queremos proteger uma variável de modificações externas, isto é, 
+impedir que ela seja alterada a não ser pela própria classe, usamos o modificador: private
 
 
-//Get obtendo valor armazenado nessa propriedade como ex:{Idade}. Set = setando/ atribuindo valor ex. = 25.
+ EX: Temos a possibilidade de implementar uma propriedade que apenas retorna um valor, não sendo possível atribuir ou modificar o valor retornado. 
+Esse tipo de propriedade é conhecida como: Propriedade somente leitura, onde implementa apenas o GET.
 
+EX2: Existem dois tipos de ações de propriedades: GET e SET. 
+Eles podem manipular variáveis ou ser auto implementadas, e suas funções são: 
+= GET: Obter o valor da propriedade; SET: Atribuir o valor da propriedade.
+*/
+
+
+
+//guia Lucas 
+/*
+propriedades:
+https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/properties
+
+métodos:
+https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/methods
+
+construtores:
+https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/constructors
+
+
+
+construtores estaticos: https://learn.microsoft.com/pt-br/dotnet/csharp/programming-guide/classes-and-structs/static-constructors
+
+*/
